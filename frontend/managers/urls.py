a01d14b863996ac9626ea4b8a3383755c448fb8a
kiddingmz <bata.isidro@gmail.com>
2024-02-15 21:56:44 +0200

    CHORE: I'm making improves the app managers

--- frontend/managers/urls.py
+++ frontend/managers/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from managers.views import index as managers_index
+from managers.views import map as managers_map
 
 
 urlpatterns = [
-    path('', managers_index, name='index'),
-    path('map/', managers_index, name='map'),
+    path('', managers_index, name='Dashboard'),
+    path('map/', managers_map, name='Map'),
 ]
