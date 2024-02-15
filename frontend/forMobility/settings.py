d66692df05b06ed2346d5284b3e7dbc7c8c565d2
kiddingmz <bata.isidro@gmail.com>
2024-02-15 19:58:08 +0200

    FEAT: Add new app named manages

--- frontend/forMobility/settings.py
+++ frontend/forMobility/settings.py
@@ -56,8 +56,7 @@ ROOT_URLCONF = 'forMobility.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -66,6 +65,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+
         },
     },
 ]
--- frontend/managers/apps.py
+++ frontend/managers/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AdministratorsConfig(AppConfig):
+class ManagersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'administrators'
+    name = 'managers'
